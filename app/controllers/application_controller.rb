244fb707295710a1010a94ce04a73bd22a911a87
Tara Bass <tbass@stardust>
2017-06-29 14:57:20 -0700

    Add some error logging to application exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from 'Exception' do |exception|
+    Rails.logger.error "ERROR: #{exception.message} #{exception.backtrace.join("\n")}"
     render json: { error: { message: 'Internal Server Error' } }, status: :ok
   end
 
